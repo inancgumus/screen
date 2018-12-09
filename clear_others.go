3220885cf55d9e5694778986960586b20601b9f5
Inanc Gumus <m@inanc.io>
2018-12-09 16:43:52 +0300

    fix: unnecessary newline addition on bashes

--- clear_others.go
+++ clear_others.go
@@ -4,13 +4,12 @@ package screen
 
 import "fmt"
 
-// Clear clears the screen and moves the cursor to the top-left
-// corner of the screen
+// Clear clears the screen
 func Clear() {
-	fmt.Println("\033[2J")
+	fmt.Print("\033[2J")
 }
 
 // MoveTopLeft moves the cursor to the top left position of the screen
 func MoveTopLeft() {
-	fmt.Println("\033[H")
+	fmt.Print("\033[H")
 }
