1251863fa0e301a9afe534ab8e755909518b82c8
154pinkchairs <mholub@tutanota.com>
2022-12-04 01:48:38 +0100

    use GetSize from term package, not crypto/ssh

--- dimensions.go
+++ dimensions.go
@@ -3,12 +3,12 @@ package screen
 import (
 	"os"
 
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 )
 
 // Size returns the width and height of the terminal screen
 func Size() (int, int) {
-	w, h, err := terminal.GetSize(int(os.Stdout.Fd()))
+	w, h, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
 		return 0, 0
 	}
